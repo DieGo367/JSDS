8bf445bcef119c8f14a00baf076ce582822f4814
DieGo <diegog367@gmail.com>
2022-09-29 19:34:16 -0700

    Error event is cancelable, aborts if not canceled

--- arm9/source/execute.cpp
+++ arm9/source/execute.cpp
@@ -49,20 +49,19 @@ jerry_value_t execute(jerry_value_t parsedCode) {
  * If left unhandled, the error will be printed and (unless in the REPL) the program will exit.
  */
 void handleError(jerry_value_t error) {
+	bool errorHandled = false;
+
 	jerry_value_t global = jerry_get_global_object();
 	jerry_value_t errorStr = jerry_create_string((jerry_char_t *) "error");
-
 	jerry_value_t eventListeners = getInternalProperty(global, "eventListeners");
 	jerry_value_t errorEventListeners = jerry_get_property(eventListeners, errorStr);
-	bool listenersExist = jerry_value_is_array(errorEventListeners) && jerry_get_array_length(errorEventListeners) > 0;
-	jerry_release_value(errorEventListeners);
-	jerry_release_value(eventListeners);
-
-	if (listenersExist) {
+	if (jerry_value_is_array(errorEventListeners) && jerry_get_array_length(errorEventListeners) > 0) {
 		jerry_value_t errorEventInit = jerry_create_object();
 
 		jerry_error_t errorCode = jerry_get_error_type(error);
 		jerry_value_t errorThrown = jerry_get_value_from_error(error, false);
+		jerry_value_t True = jerry_create_boolean(true);
+		setProperty(errorEventInit, "cancelable", True);
 		jerry_release_value(jerry_set_property(errorEventInit, errorStr, errorThrown));
 		if (errorCode == JERRY_ERROR_NONE) {
 			jerry_value_t strVal = jerry_value_to_string(errorThrown);
@@ -104,23 +103,24 @@ void handleError(jerry_value_t error) {
 		jerry_release_value(ErrorEvent);
 		jerry_release_value(errorEventInit);
 
-		jerry_value_t True = jerry_create_boolean(true);
 		setInternalProperty(errorEvent, "isTrusted", True);
 		jerry_release_value(True);
 
 		jerry_value_t dispatchFunc = getProperty(global, "dispatchEvent");
 		jerry_value_t result = jerry_call_function(dispatchFunc, global, &errorEvent, 1);
 		if (jerry_value_is_error(result)) handleError(result); // lol
+		else if (jerry_get_boolean_value(result) == false) errorHandled = true;
 		jerry_release_value(result);
 		jerry_release_value(dispatchFunc);
 
 		jerry_release_value(errorEvent);
 	}
-
+	jerry_release_value(errorEventListeners);
+	jerry_release_value(eventListeners);
 	jerry_release_value(errorStr);
 	jerry_release_value(global);
 
-	if (!listenersExist) {
+	if (!errorHandled) {
 		consolePrintLiteral(error);
 		putchar('\n');
 		if (!inREPL) { // if not in the REPL, abort on uncaught error. Waits for START like normal.
